97758ba2ce80b8bc0a5f2eb92d21ff1f47390d45
jjosegallegomesa <juan.gallegomesa@gmail.com>
2022-05-02 18:30:01 -0500

    se crea proyecto

---
+++ controlador.js
@@ -0,0 +1,130 @@
+//OBJETOS
+let producto1={
+    nombre:"producto1",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto2={
+    nombre:"producto2",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto3={
+    nombre:"producto3",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto4={
+    nombre:"producto4",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto5={
+    nombre:"producto5",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto6={
+    nombre:"producto6",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto7={
+    nombre:"producto7",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto8={
+    nombre:"producto8",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto9={
+    nombre:"producto9",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto10={
+    nombre:"producto10",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto11={
+    nombre:"producto11",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto12={
+    nombre:"producto12",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto13={
+    nombre:"producto13",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto14={
+    nombre:"producto14",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto15={
+    nombre:"producto15",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+//ARREGLO
+let productos=Array(
+    producto1,
+    producto2,
+    producto3,
+    producto4,
+    producto5,
+    producto6,
+    producto7,
+    producto8,
+    producto9,
+    producto10,
+    producto11,
+    producto12,
+    producto13,
+    producto14,
+    producto15
+)
+
+//RECORRO EL ARREGLO
+productos.forEach(function(producto){
+    console.log(producto.nombre)
+})
+
