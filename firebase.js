38dd47b1cbaa1fcdd1f55b984d5c270cec4b4991
Your Name <you@example.com>
2022-05-03 15:50:34 -0500

    mercado

---
+++ firebase.js
@@ -0,0 +1,23 @@
+  /*
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyA1jcZVkPWtsPVX3TV8OOxiIvTd78Eh91M",
+    authDomain: "la-tiendita-b5b49.firebaseapp.com",
+    projectId: "la-tiendita-b5b49",
+    storageBucket: "la-tiendita-b5b49.appspot.com",
+    messagingSenderId: "294423967824",
+    appId: "1:294423967824:web:cc83c04a6a36cb40187955",
+    measurementId: "G-S81CQJTNNH"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+*/
